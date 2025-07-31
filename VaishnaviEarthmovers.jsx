ea7aef57a67b4f17b829471933cb617ef4575e64
Lalit Joshi <91650002+lalit-hld@users.noreply.github.com>
2025-08-01 00:26:04 +0530

    Update VaishnaviEarthmovers.jsx

--- VaishnaviEarthmovers.jsx
+++ VaishnaviEarthmovers.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import LanguageToggle from './components/LanguageToggle';
+import ContactForm from './components/ContactForm';
+import MachineGallery from './components/MachineGallery';
+import Footer from './components/Footer';
+
+function VaishnaviEarthmovers() {
+  return (
+    <div>
+      <LanguageToggle />
+      <header className="header">
+        <h1>Vaishnavi Earthmovers and Transports</h1>
+        <p>Excavator, Loader, Backhoe Loader, Dumper & Trala for Rent</p>
+      </header>
+      <MachineGallery />
+      <ContactForm />
+      <Footer />
+    </div>
+  );
+}
+
 export default VaishnaviEarthmovers;
